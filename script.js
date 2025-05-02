74978a6a8e25bfdb0c93593a1499ebff386f6849
Star <email170512@gmail.com>
2025-05-02 17:59:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toolsBtn = document.getElementById("toolsbtn");
+
+    toolsBtn.addEventListener("click", function () {
+        window.location.href = "tools.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const creditsBtn = document.getElementById("creditsbtn");
+    creditsBtn.addEventListener("click", function () {
+        window.location.href = "credits.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const homebtn = document.getElementById("homebtn");
+    homebtn.addEventListener("click", function () {
+        window.location.href = "index.html";
+    });
+});
